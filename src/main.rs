623b3aa0f0bcc3169461ecf4c426d89780c7225f
AWebb5225 <alexander.webber5225@gmail.com>
2024-03-29 13:12:43 -0700

    added extra features

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,33 @@ use rand::Rng;
 fn main() {
     println!("Guess a number!"); //macro to print to stdout
     loop {
-    println!("Enter your guess: ");
-    let mut guess = String::new(); //let creates new variable guess "mut" makes it mutable
-    io::stdin().read_line(&mut guess)
+        println!("Enter your guess: ");
+        let mut guess = String::new(); //let creates new variable guess "mut" makes it mutable
+        io::stdin().read_line(&mut guess)
                  .expect("Failed to read line"); //read into the address of guess
-    let guess: u32 = guess.trim().parse()
-                 .expect("Please type a number!");
-    println!("You guess: {}", guess);
-    let rand_num = rand::thread_rng().gen_range(1,100);
-    println!("The random number is: {}", rand_num);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guess: {}", guess);
+        let rand_num = rand::thread_rng().gen_range(1,100);
+        println!("The random number is: {}", rand_num);
     
-    match guess.cmp(&rand_num) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("Just Right!"),
+        match guess.cmp(&rand_num) {
+            Ordering::Less => {
+                println!("Too small!");
+                println!("The random number was: {}", rand_num);
+            },
+            Ordering::Greater => {
+                println!("Too big!");
+                println!("The random number was: {}", rand_num);
+            },
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
-}
 }
